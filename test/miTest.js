21629461fc780690ab79ca1addd6aba9d06e8b71
FORGISDELUXE <jorgesolgonzalez1998@gmail.com>
2019-09-26 13:56:18 +0200

    mejorando poco pero algo el test

--- test/miTest.js
+++ test/miTest.js
@@ -2,10 +2,21 @@ const Productos = artifacts.require("Productos");
 
 contract("Productos", async (accounts) => {
 
-it ("Añadir 1 producto", async() => {
+	it ("Añadir 2 frutas, 2 animales, 2 verduras...", async() => {
 
-	let instance = await Productos.deployed();
-	instance._addProduct("Manzana", "Fruta", 1);
-});
+		let instance = await Productos.deployed();
+		instance._addProduct("Manzana", "Fruta", 1);
+		instance._addProduct("Naranja", "Fruta", 2);
+		instance._addProduct("Cerdo", "Animales", 3);
+		instance._addProduct("Vaca", "Animales", 4);
+		instance._addProduct("Lechuga", "Verduras", 5);
+		instance._addProduct("Brocoli", "Verduras", 6);
+	
+	});
+
+	it ("Buscar por nombre...", async() => {
+		let instance = await Productos.deployed();
+	
+	});
 
 });
